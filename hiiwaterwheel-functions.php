0e58cdea6639134ef51b79911407acf93f1539f5
heather-anne <heather@hiilite.com>
2018-03-27 12:42:17 -0700

    Update to function names

--- hiiwaterwheel-functions.php
+++ hiiwaterwheel-functions.php
@@ -1,38 +1,10 @@
 <?php
-	
-/**
- * Get a list of terms
- *
- * Generic function to return an array of taxonomy terms formatted for CMB2.
- * Simply pass in your get_terms arguments and get back a beautifully formatted
- * CMB2 options array.
- *
- * @param string|array $taxonomies Taxonomy name or list of Taxonomy names
- * @param  array|string $query_args Optional. Array or string of arguments to get terms
- * @return array CMB2 options array
- */
-function iweb_get_cmb_options_array_tax( $taxonomies, $query_args = '' ) {
-	$defaults = array(
-		'hide_empty' => false
-	);
-	$args = wp_parse_args( $query_args, $defaults );
-	$terms = get_terms( $taxonomies, $args );
-	$terms_array = array();
-	if ( ! empty( $terms ) ) {
-		foreach ( $terms as $term ) {
-			$terms_array[$term->term_id] = $term->name;
-		}
-	}
-	return $terms_array;
-}
-
-
 /**
  * 
  * Add shortcode column to waterwheel in dashboard
  * 
  **/
-function waterwheel_shortcode_column($columns) {
+function hii_waterwheel_shortcode_column($columns) {
   $new = array();
   foreach($columns as $key => $title) {
     if ($key=='date') // Put the Thumbnail column before the Author column
@@ -41,14 +13,14 @@ function waterwheel_shortcode_column($columns) {
   }
   return $new;
 }
-add_filter('manage_waterwheel_posts_columns', 'waterwheel_shortcode_column');
+add_filter('manage_waterwheel_posts_columns', 'hii_waterwheel_shortcode_column');
 
 
-function waterwheel_shortcode($column_name, $post_id) {
+function hii_waterwheel_shortcode($column_name, $post_id) {
     if ($column_name == 'shortcode') {
         echo '[hiiww id="'.$post_id.'"]';
     }
 }
-add_action('manage_waterwheel_posts_custom_column', 'waterwheel_shortcode', 10, 2);
+add_action('manage_waterwheel_posts_custom_column', 'hii_waterwheel_shortcode', 10, 2);
 
 
